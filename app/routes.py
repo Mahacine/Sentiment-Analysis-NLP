91e919f1b0c692bc014ec820a08cf13ac813451d
MahacineEttahri <mahacine.ma@baybridgedigital.com>
2024-05-12 04:36:22 +0100

    Added NLTK analysis to the front-end

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,18 @@
 from app import app
 from flask import render_template
 from app.form import TextForm
+from main_nltk import analyze
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+#@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = TextForm()
     if form.validate_on_submit():
         text = form.text.data
-        return "Success"
+        # Call your method to analyze the text
+        analysis_result = analyze(text)
+        # Populate the answer field with the result
+        form.answer.data = analysis_result
+        # Render the template with the updated form
+        return render_template('index.html', title="Sentiment analysis", form=form)
     return render_template('index.html', title="Sentiment analysis", form=form)
