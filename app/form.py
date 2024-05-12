799445dce599866e8a65a6a3cc168d4b44903eb1
MahacineEttahri <mahacine.ma@baybridgedigital.com>
2024-05-12 05:26:28 +0100

    Added manual analysis to the front-end

--- app/form.py
+++ app/form.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
+from wtforms import TextAreaField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class TextForm(FlaskForm):
     text = TextAreaField('How are you feeling today?', validators=[DataRequired()], render_kw={"rows": 5})
     submit = SubmitField('Analyze')
     answer = TextAreaField('Analysis Result', render_kw={"rows": 5, "readonly": True})
+    method = SelectField('Choose analysis method', choices=[('nltk', 'NLTK'), ('manual', 'Manual')], validators=[DataRequired()])
+    plot = TextAreaField('Plot', render_kw={"style": "display:none;"})  # Hidden field for the plot data
